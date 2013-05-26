5de39874e3b276f885dee3384b0d984513ad95b8
perandersson <dim.raven@gmail.com>
2013-05-26 18:07:48 +0200

    Add build type so that test-cases are not part of the build-chain. Added test cases for the scene.
    This is because the application must be built successfully for it to be
    debuggable, i.e. you cannot debug the playstate.test without disabling
    tests.

--- playstate.tests/playstate/scene/scene_test.cpp
+++ playstate.tests/playstate/scene/scene_test.cpp
@@ -1,6 +1,90 @@
 #include <playstate/scene/scene.h>
 #include "../test.h"
+#include <vector>
 using namespace playstate;
+
+class MockRenderProcessor : public IRenderProcessor
+{
+public:
+	virtual void AttachRenderable(Renderable* renderable)
+	{
+		items.push_back(renderable);
+	}
+	
+	virtual void DetachRenderable(Renderable* renderable)
+	{
+		std::vector<Renderable*>::iterator it = std::find(items.begin(), items.end(), renderable);
+		items.erase(it);
+	}
+
+	virtual bool Find(const FindQuery& query, FindResultSet<RenderBlock>* target) const
+	{
+		
+		return true;
+	}
+
+	std::vector<Renderable*> items;
+};
+
+class MockRenderProcessorFactory : public IRenderProcessorFactory
+{
+public:
+	virtual IRenderProcessor* Create() const
+	{
+		return new MockRenderProcessor();
+	}
+};
+
+class MockUpdateProcessor : public IUpdateProcessor
+{
+public:
+	virtual void AttachUpdatable(IUpdatable* updatable)
+	{
+	}
+
+	virtual void DetachUpdatable(IUpdatable* updatable)
+	{
+	}
+
+	virtual void Update()
+	{
+	}
+};
+
+class MockUpdateProcessFactory : public IUpdateProcessorFactory
+{
+public:
+	virtual IUpdateProcessor* Create() const
+	{
+		return new MockUpdateProcessor();
+	}
+};
+
+class MockRenderableComponent : public Component, public Renderable
+{
+public:
+	MockRenderableComponent()
+	{
+	}
+
+	virtual ~MockRenderableComponent()
+	{
+	}
+
+	virtual void OnComponentAdded()
+	{
+		Renderable::Attach(Owner->Group);
+	}
+
+	virtual void OnComponentRemoved()
+	{
+		Renderable::Detach();
+	}
+
+	virtual void CollectBuildingBlocks(RenderBlockBuilder& builder, RenderState& state)
+	{
+	}
+};
 
 TestCase(Scene_FindNonExistingItems, "Scene.Find(...) == false")
 {
@@ -9,4 +93,22 @@ TestCase(Scene_FindNonExistingItems, "Scene.Find(...) == false")
 
 	FindResultSet<RenderBlock> resultSet;
 	AssertFalse(scene.Find(query, &resultSet));
+}
+
+TestCase(Scene_FindRenderableItems, "Scene.Find(NULL, TempType) == true")
+{
+	MockUpdateProcessFactory updateProcessorFactory;
+	MockRenderProcessorFactory renderProcessorFactory;
+
+	Scene scene;
+	SceneGroup* group = new SceneGroup(updateProcessorFactory, renderProcessorFactory);
+	SceneNode* node1 = new SceneNode(group);
+	node1->AddComponent(new MockRenderableComponent());
+	SceneNode* node2 = new SceneNode(group);
+	scene.AddSceneGroup(group);
+	
+	const FindQuery query = { NULL, 0 };
+	
+	FindResultSet<RenderBlock> resultSet;
+	AssertTrue(scene.Find(query, &resultSet));
 }
