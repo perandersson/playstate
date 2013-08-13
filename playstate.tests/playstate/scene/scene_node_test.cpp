666919e566bd94b370dcb9710fe38d294edb2f18
perandersson <dim.raven@gmail.com>
2013-08-13 07:12:29 +0200

    preparations for node test cases.

---
+++ playstate.tests/playstate/scene/scene_node_test.cpp
@@ -0,0 +1,16 @@
+#include <playstate/scene/scene_node.h>
+#include "../test/test.h"
+using namespace playstate;
+
+TEST_SUITE(SceneNode)
+{
+	UNIT_TEST(AddAndListNode)
+	{
+		ASSERT_TRUE(false);
+	}
+
+	UNIT_TEST(AddComponentsAndRemoveNode)
+	{
+		ASSERT_TRUE(false);
+	}
+}
