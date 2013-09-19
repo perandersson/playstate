a52d9b7931f599cb2c0913d42e13bacadb342c73
perandersson <dim.raven@gmail.com>
2013-09-19 18:39:29 +0200

    Added min and max functions.

--- playstate/playstate/math/math.h
+++ playstate/playstate/math/math.h
@@ -13,5 +13,17 @@ namespace playstate
 		// @param v1 The second value
 		// @param t Delta between [0, 1]
 		static float32 Lerp(float32 v0, float32 v1, float32 t);
+
+		//
+		// Return the maximum value of the two supplied values
+		template<typename T>
+		static T Max(T val1, T val2) {
+			return val1 > val2 ? val1 : val2;
+		}
+
+		template<typename T>
+		static T Min(T val1, T val2) {
+			return val1 < val2 ? val1 : val2;
+		}
 	};
 }
