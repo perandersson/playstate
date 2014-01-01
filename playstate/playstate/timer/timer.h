e59fe4bfe7e189a24b7d4c88dcf648bd2b84d436
perandersson <dim.raven@gmail.com>
2014-01-02 00:22:11 +0100

    Added comments.

--- playstate/playstate/timer/timer.h
+++ playstate/playstate/timer/timer.h
@@ -10,12 +10,22 @@ namespace playstate
 		virtual ~ITimer() {}
 
 	public:
+		//
+		// Starts this timer
 		virtual void Start() = 0;
 
+		//
+		// Returns the time it's been since the timer started and restart it afterwards.
+		//
+		// @return The time in seconds since the timer was started
 		virtual float32 GetElapsedAndRestart() = 0;
 
+		//
+		// @return The total time in seconds between one start and one stop
 		virtual float64 GetTotalTime() = 0;
 
+		//
+		// Stops this timer
 		virtual void Stop() = 0;
 	};
 }
